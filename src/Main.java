b7e3e8fee8e95ed38f8d85ed4e42de410b655a97
emilbekovna <n.abdyraeva25@gmail.com>
2022-07-06 11:13:24 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Nurzhan nurzhan = new Nurzhan();
+        nurzhan.setName("Nurzhan");
+        System.out.println(nurzhan.getName());
+    }
+}
---
+++ src/Nurzhan.java
@@ -0,0 +1,12 @@
+public class Nurzhan {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
